9042adcbe811b70b55c7b65f1e8132061b4d4863
fuxianjin <fuxianjin-hj@huajiao.tv>
2018-04-26 16:47:05 +0800

    给权限申请回调接口PermissionListener添加相应注释

--- app/src/main/java/com/chaychan/news/listener/PermissionListener.java
+++ app/src/main/java/com/chaychan/news/listener/PermissionListener.java
@@ -7,8 +7,8 @@ import java.util.List;
  * @description: 权限申请回调的接口
  */
 public interface PermissionListener {
-
+    /**授予权限回调方法*/
     void onGranted();
-
+    /**拒绝权限回到方法*/
     void onDenied(List<String> deniedPermissions);
 }
